06ffeb190e4592552e71ee839a759ff40b7d1b8e
Ewerton Dutra <ewerton-speed@hotmail.com>
2024-12-31 22:59:33 -0300

    Corrigido erros com caracteres especiais nas pastas do windows e possivelmente linux também

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,8 +1,14 @@
 import slugify from "slugify";
 
+const options = {
+    replacement: '-',  // Substitui os caracteres não permitidos por '-'
+    remove: /[\/\+\(\)\[\]\{\}<>!@#$%^&*=_|:]/g,
+    lower: true,       // Converte para minúsculas
+};
+
 export const courses = [
     {
-        id: slugify("Go para iniciantes: Uma introdução a linguagem do Google", { lower: true }),
+        id: slugify("Go para iniciantes: Uma introdução a linguagem do Google", options),
         title: "Go para iniciantes: Uma introdução a linguagem do Google",
         description: "Fundamentos teóricos, desafios práticos e SUPORTE garantido para uma aprendizagem completa.",
         image: "/images/cursos/go-para-iniciantes-uma-introducao-a-linguagem-do-google.webp",
@@ -10,7 +16,7 @@ export const courses = [
         label: "Curso Go Iniciante",
     },
     {
-        id: slugify("Curso Flutter completo/Atualizado 2024 Android/iOS/Windows", { lower: true }),
+        id: slugify("Curso Flutter completo/Atualizado 2024 Android/iOS/Windows", options),
         title: "Curso Flutter completo/Atualizado 2024 Android/iOS/Windows",
         description: "Curso completo de Flutter e Dart - Aprenda desde o básico até aplicativos práticos para Android, iOS, Windows e Mac.",
         image: "/images/cursos/curso-flutter-completo-atualizado-2024-android-ios-windows.webp",
@@ -18,7 +24,7 @@ export const courses = [
         label: "Curso Flutter Completo",
     },
     {
-        id: slugify("Laravel 11 e Flutter 3 aplicações com comunicação real-time 2024 - Impressor", { lower: true }),
+        id: slugify("Laravel 11 e Flutter 3 aplicações com comunicação real-time 2024 - Impressor", options),
         title: "Laravel 11 e Flutter 3 aplicações com comunicação real-time 2024 - Impressor",
         description: "Curso completo de Flutter e Dart - Aprenda desde o básico até aplicativos práticos para Android, iOS, Windows e Mac.",
         image: "/images/cursos/laravel-11-e-flutter-3-aplicacoes-realtime-2024-impressor.webp",
@@ -26,7 +32,7 @@ export const courses = [
         label: "Curso Flutter e Laravel - RealTime",
     },
     {
-        id: slugify("Projeto Delivery Hortifruti c/ Flutter 3 + AdonisJS 5 NodeJS", { lower: true }),
+        id: slugify("Projeto Delivery Hortifruti c/ Flutter 3 + AdonisJS 5 NodeJS", options),
         title: "Projeto Delivery Hortifruti c/ Flutter 3 + AdonisJS 5 NodeJS",
         description: "Aprenda a criar um aplicativo de delivery de hortifruti com Flutter 3 e um backend com AdonisJS 5.",
         image: "/images/cursos/projeto-delivery-hortifruti-com-flutter-3-adonisjs-5-nodejs.webp",
