35082a641afe80d0aaccfc2fd7c5fc93c3d615a2
Ewerton Dutra <ewerton-speed@hotmail.com>
2025-02-01 18:32:39 -0300

    Atualizado os links de promoções do mês

--- src/data/courses.js
+++ src/data/courses.js
@@ -12,15 +12,15 @@ export const courses = [
         title: "Go para iniciantes: Uma introdução a linguagem do Google",
         description: "Fundamentos teóricos, desafios práticos e SUPORTE garantido para uma aprendizagem completa.",
         image: "/images/cursos/go-para-iniciantes-uma-introducao-a-linguagem-do-google.webp",
-        link: "https://www.udemy.com/course/aprenda-go-aka-golang-a-linguagem-do-google/?couponCode=F59712479318-2025JAN",
+        link: "https://www.udemy.com/course/aprenda-go-aka-golang-a-linguagem-do-google/?couponCode=268D2ADC0E-FEV2025",
         label: "Curso Go Iniciante",
     },
     {
-        id: slugify("Curso Flutter completo/Atualizado 2024 Android/iOS/Windows", options),
-        title: "Curso Flutter completo/Atualizado 2024 Android/iOS/Windows",
+        id: slugify("Curso Flutter completo/Atualizado 2025 Android/iOS/Windows", options),
+        title: "Curso Flutter completo/Atualizado 2025 Android/iOS/Windows",
         description: "Curso completo de Flutter e Dart - Aprenda desde o básico até aplicativos práticos para Android, iOS, Windows e Mac.",
         image: "/images/cursos/curso-flutter-completo-atualizado-2024-android-ios-windows.webp",
-        link: "https://www.udemy.com/course/curso-de-flutter-e-dart-completo-atualizado/?couponCode=CC3CAA22B8-2025JAN",
+        link: "https://www.udemy.com/course/curso-de-flutter-e-dart-completo-atualizado/?couponCode=9C9973E6CA6-FEV2025",
         label: "Curso Flutter Completo",
     },
     {
@@ -28,7 +28,7 @@ export const courses = [
         title: "Laravel 11 e Flutter 3 aplicações com comunicação real-time 2024 - Impressor",
         description: "Curso completo de Flutter e Dart - Aprenda desde o básico até aplicativos práticos para Android, iOS, Windows e Mac.",
         image: "/images/cursos/laravel-11-e-flutter-3-aplicacoes-realtime-2024-impressor.webp",
-        link: "https://www.udemy.com/course/laravel-11-flutter-3-aplicacoes-tempo-real-impressor-delivery-desktop/?couponCode=FEBDD846F7F-2025JAN",
+        link: "https://www.udemy.com/course/laravel-11-flutter-3-aplicacoes-tempo-real-impressor-delivery-desktop/?couponCode=7A22BC8F19AF-FEV2025",
         label: "Curso Flutter e Laravel - RealTime",
     },
     {
@@ -36,7 +36,7 @@ export const courses = [
         title: "Projeto Delivery Hortifruti c/ Flutter 3 + AdonisJS 5 NodeJS",
         description: "Aprenda a criar um aplicativo de delivery de hortifruti com Flutter 3 e um backend com AdonisJS 5.",
         image: "/images/cursos/projeto-delivery-hortifruti-com-flutter-3-adonisjs-5-nodejs.webp",
-        link: "https://www.udemy.com/course/curso-flutter-3-e-getx-api-adonisjs-projeto-hortifruti/?couponCode=8F24E6F0F3-2025JAN",
+        link: "https://www.udemy.com/course/curso-flutter-3-e-getx-api-adonisjs-projeto-hortifruti/?couponCode=4F59BE2DDB55-FEV2025",
         label: "Curso Flutter e AdonisJS",
     },
 ];
