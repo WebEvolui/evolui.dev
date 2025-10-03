b8d0641b322bc734c3aa1f91a9305ecbb45c3cae
Ewerton Dutra <ewerton-speed@hotmail.com>
2025-10-03 17:35:48 -0300

    Links dos cursos atualizados E também o astro atualizado

--- src/data/courses.js
+++ src/data/courses.js
@@ -20,7 +20,7 @@ export const courses = [
         title: "Curso Django REST Framework: crie APIs com Python",
         description: "Aprenda como criar aplicações REST com Python usando o DRF (Django REST Framework). Sua API de maneira fácil e segura",
         image: "/images/cursos/curso-django-rest-framework-crie-apis-com-python.webp",
-        link: "https://www.udemy.com/course/curso-django-rest-framework-crie-apis-com-python/?couponCode=D708B75082-SET2025",
+        link: "https://www.udemy.com/course/curso-django-rest-framework-crie-apis-com-python/?couponCode=FBEE595AE98-OUT2025",
         label: "Curso Django REST Framework",
     },
     {
@@ -28,7 +28,7 @@ export const courses = [
         title: "Quasar 2 framework: construindo um mini ERP online com API",
         description: "Quasar Framework é um framework de código aberto baseado em Vue.js para construir aplicativos com uma única code base",
         image: "/images/cursos/quasar2-framework-construindo-mini-erp-online-com-api.png",
-        link: "https://www.udemy.com/course/desenvolvimento-web-construindo-aplicacao-de-maneira-rapida/?couponCode=4E4A3ADE5D3-SET2025",
+        link: "https://www.udemy.com/course/desenvolvimento-web-construindo-aplicacao-de-maneira-rapida/?couponCode=5C01EAFD5999-OUT2025",
         label: "Curso Quasar",
     },
     {
@@ -36,7 +36,7 @@ export const courses = [
         title: "Go para iniciantes: Uma introdução a linguagem do Google",
         description: "Fundamentos teóricos, desafios práticos e SUPORTE garantido para uma aprendizagem completa.",
         image: "/images/cursos/go-para-iniciantes-uma-introducao-a-linguagem-do-google.webp",
-        link: "https://www.udemy.com/course/aprenda-go-aka-golang-a-linguagem-do-google/?couponCode=D6390A35AD5-SET2025",
+        link: "https://www.udemy.com/course/aprenda-go-aka-golang-a-linguagem-do-google/?couponCode=689D25C99E-OUT2025",
         label: "Curso Go Iniciante",
     },
     {
@@ -44,7 +44,7 @@ export const courses = [
         title: "Curso Flutter completo/Atualizado 2025 Android/iOS/Windows",
         description: "Curso completo de Flutter e Dart - Aprenda desde o básico até aplicativos práticos para Android, iOS, Windows e Mac.",
         image: "/images/cursos/curso-flutter-completo-atualizado-2024-android-ios-windows.webp",
-        link: "https://www.udemy.com/course/curso-de-flutter-e-dart-completo-atualizado/?couponCode=455B2D947E8B-SET2025",
+        link: "https://www.udemy.com/course/curso-de-flutter-e-dart-completo-atualizado/?couponCode=FA5DF02C2B23-OUT2025",
         label: "Curso Flutter Completo",
     },
     {
@@ -52,7 +52,7 @@ export const courses = [
         title: "Laravel 12 e Flutter 3 aplicações com comunicação real-time 2025 - Impressor",
         description: "Domine Flutter e Laravel com foco em comunicação em tempo real, criando aplicações modernas e integradas para múltiplas plataformas.",
         image: "/images/cursos/laravel-11-e-flutter-3-aplicacoes-realtime-2024-impressor.webp",
-        link: "https://www.udemy.com/course/laravel-11-flutter-3-aplicacoes-tempo-real-impressor-delivery-desktop/?couponCode=C20535C2FFC-SET2025",
+        link: "https://www.udemy.com/course/laravel-11-flutter-3-aplicacoes-tempo-real-impressor-delivery-desktop/?couponCode=CF6BEAFA5-OUT2025",
         label: "Curso Flutter e Laravel - RealTime",
     },
     {
@@ -60,7 +60,7 @@ export const courses = [
         title: "Projeto Delivery Hortifruti c/ Flutter 3 + AdonisJS 5 NodeJS",
         description: "Aprenda a criar um aplicativo de delivery de hortifruti com Flutter 3 e um backend com AdonisJS 5.",
         image: "/images/cursos/projeto-delivery-hortifruti-com-flutter-3-adonisjs-5-nodejs.webp",
-        link: "https://www.udemy.com/course/curso-flutter-3-e-getx-api-adonisjs-projeto-hortifruti/?couponCode=E50A7B2C155-SET2025",
+        link: "https://www.udemy.com/course/curso-flutter-3-e-getx-api-adonisjs-projeto-hortifruti/?couponCode=6326035C246-OUT2025",
         label: "Curso Flutter e AdonisJS",
     },
 ];
